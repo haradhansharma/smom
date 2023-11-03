6cf3cafe2a2c4a4061f33aa89a59730d5a561201
haradhansharma <haradhan.sharma@gmail.com>
2023-11-03 23:59:37 +0600

    first commit

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+# Import necessary modules
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-application = get_wsgi_application()
+# Check the operating system name
+if os.name != 'nt':
+    # On non-Windows systems, configure the Python path and Django settings
+    sys.path.insert(0, '/home/veerera/smomenv/lib/python3.9/site-packages')
+    from django.core.wsgi import get_wsgi_application
+    sys.path.insert(1, '/home/veerera/sankarmath_html')
+    os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
+    application = get_wsgi_application()        
+else:   
+    # On Windows systems, configure Django settings 
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
